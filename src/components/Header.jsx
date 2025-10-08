f5edb1fde1d97e323699602aca35c07c5b89af2c
sabeeh <sabeehck02@gmail.com>
2025-10-08 23:29:57 +0530

    Commit 1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="src\assets\globe.png" alt="Globe Icon"></img>
+            <span>my travel journal.</span>
+        </header>
+    );
+}
