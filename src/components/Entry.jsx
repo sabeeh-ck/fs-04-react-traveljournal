2ee2298a21a50552a5f1ba00dbcf7456bbf4193c
sabeeh <sabeehck02@gmail.com>
2025-10-15 00:43:37 +0530

    Commit 3.0

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,21 +1,16 @@
-export default function Entry() {
+export default function Entry(props) {
     return (
         <article>
-            <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount Fuji" />
+            <img src={props.img.src} alt={props.img.alt} />
             <div className="details">
                 <div className="location">
                     <img src="src\assets\marker.png" alt="" />
-                    <span>JAPAN</span>
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-                        VIew on Google Maps
-                    </a>
+                    <span>{props.country}</span>
+                    <a href={props.googleMapsLink}>VIew on Google Maps</a>
                 </div>
-                <h1>Mount Fuji</h1>
-                <span className="date">12 Jan, 2023 - 24 Jan, 2023</span>
-                <p>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji
-                    is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
-                </p>
+                <h1>{props.title}</h1>
+                <span className="date">{props.dates}</span>
+                <p>{props.text}</p>
             </div>
         </article>
     );
